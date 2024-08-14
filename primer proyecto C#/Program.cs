fbc3f4f4254b0325008e0a1a0afad3fd131bb344
ChristopherMaldonado24 <saidreyes711@gmail.com>
2024-08-14 14:41:08 -0600

    Actualizacion
    cambio completo

--- primer proyecto C#/Program.cs
+++ primer proyecto C#/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola Mundo");
+﻿
+
+int EdadMeses, Edad;
+string Nombre;
+Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
+Console.WriteLine("Ingresa tu nombre para continuar");
+Nombre = Console.ReadLine();//Para recibir valores de caracter
+Console.WriteLine("ingrese su edad");
+Edad = Convert.ToInt32(Console.ReadLine);
+EdadMeses = Edad * 12;
