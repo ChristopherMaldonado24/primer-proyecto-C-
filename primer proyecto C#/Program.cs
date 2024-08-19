d6f36add87ba92654cc03027e599d93d309e7c44
ChristopherMaldonado24 <saidreyes711@gmail.com>
2024-08-19 14:06:51 -0600

    Actualizacion
    Cambios del codigo

--- primer proyecto C#/Program.cs
+++ primer proyecto C#/Program.cs
@@ -1,10 +1,24 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
 
-int EdadMeses, Edad;
-string Nombre;
-Console.WriteLine("Bienvenidos a nuestro primer sistema creado en C#");
-Console.WriteLine("Ingresa tu nombre para continuar");
-Nombre = Console.ReadLine();//Para recibir valores de caracter
-Console.WriteLine("ingrese su edad");
-Edad = Convert.ToInt32(Console.ReadLine);
-EdadMeses = Edad * 12;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenidos a nuestro primer sistema creado por github");
+
+        Console.WriteLine("Ingrese su nombre para continuar");
+        string Nombre = Console.ReadLine(); //Para recibir valores de caracter
+        Console.WriteLine("Ingrese su edad");
+        int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
+        double EdadMeses = Edad * 12; //Calculos Matematicos
+        Console.WriteLine(Nombre + " Tu edad en Meses es: " + EdadMeses + " Meses");//concatenacion
+        if (Edad > 18)
+        {
+            Console.WriteLine(" Usted es Mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine(" Usted es Menor de edad");
+        }
+    }
+}
